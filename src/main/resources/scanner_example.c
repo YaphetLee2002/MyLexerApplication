759bad1150cff8463d052e9a3f52d62af5055de5
skooch02 <lyy.skooch02@gmail.com>
2023-04-19 17:16:52 +0800

    Lab 5 update

--- src/main/resources/scanner_example.c
+++ src/main/resources/scanner_example.c
@@ -1,6 +1,18 @@
-// 注释测试，如果看到了这行字，说明预处理失败了
-/* 注释测试，如果看到了这行字，说明预处理失败了 */
-int sum(int a, int b)
-{
-    return a + b;
+int fibonacci (int n) {
+    if (n < 2)
+        return n;
+    int f = 0, g = 1;
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+        result = f + g;
+        f = g;
+        g = result;
+    }
+    return result;
+}
+int main() {
+    int a = 10;
+    int res = fibonacci(a);
+    _OUTPUT(res);
+    return 0;
 }
